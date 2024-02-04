9280b2bd423b9b769c5572572be3fb67c8c3fb33
cswebdev <csnider91@live.com>
2024-02-03 19:13:15 -0500

    created a hero section and imported some images

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Hero.css";
+import heroIllustration from "../../assets/images/hero-illustration.png";
+import reactIcon from "../../assets/images/react-icon.png";
+import javaScriptIcon from "../../assets/images/javaScript-icon.png";
+import AngularIcon from "../../assets/images/angular-icon.png";
+import htmlIcon from "../../assets/images/html5.png";
+import cssIcon from "../../assets/images/css3.png";
+
+const Hero = () => {
+   return (
+      <>
+         <section className="hero-container">
+            <div className="hero-content">
+               <h2>Building Digital Experiences that Inspire</h2>
+               <p>
+                  Passionate Full Stack Developer | Transforming ideas into
+                  seamless and visually stunning web solutions
+               </p>
+            </div>
+
+            <div className="hero-img">
+               <div>
+                  <img src={heroIllustration} alt="" />
+               </div>
+               <img src={reactIcon} alt="" />
+            </div>
+         </section>
+      </>
+   );
+};
+
+export default Hero;
