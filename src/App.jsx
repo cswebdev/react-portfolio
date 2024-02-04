0ebf7c809f7e984aff0abda4f1d47270993f4f91
cswebdev <csnider91@live.com>
2024-02-04 14:57:42 -0500

    created work history data and imported and rendered data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
 import Skills from "./components/Skills/Skills";
+import WorkExperience from "./components/WorkExperience/WorkExperience";
 
 const App = () => {
    return (
@@ -11,6 +12,7 @@ const App = () => {
          <div className="container">
             <Hero />
             <Skills />
+            <WorkExperience />
          </div>
       </>
    );
