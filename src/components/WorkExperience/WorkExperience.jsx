dc218adcfc414af4bb07b212478756d6204a1434
cswebdev <csnider91@live.com>
2024-02-04 20:57:31 -0500

    implemented react-slider and added responsiveness

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -1,18 +1,59 @@
 import React from "react";
+import { useRef } from "react";
+import Slider from "react-slick";
 import "./WorkExperience.css";
 import { WORK_EXPERIENCE } from "../../utils/data";
 import ExperienceCard from "./ExperienceCard/ExperienceCard";
 
 const WorkExperience = () => {
+   const sliderRef = useRef();
+   const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      responsive: [
+         {
+            breakpoint: 769,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            },
+         },
+      ],
+   };
+
+   const slideRight = () => {
+      sliderRef.current.slickNext();
+   };
+
+   const slideLeft = () => {
+      sliderRef.current.slickPrev();
+   };
    return (
       <>
          <section className="experience-container">
             <h5>Work Experience</h5>
 
             <div className="experience-content">
-               {WORK_EXPERIENCE.map((item) => (
-                  <ExperienceCard key={item.title} details={item} />
-               ))}
+               <div className="arrow-right" onClick={slideRight}>
+                  <span className="material-symbols-outlined">
+                     chevron_right
+                  </span>
+               </div>
+               <div className="arrow-left" onClick={slideLeft}>
+                  <span className="material-symbols-outlined">
+                     chevron_left
+                  </span>
+               </div>
+
+               <Slider ref={sliderRef} {...settings}>
+                  {WORK_EXPERIENCE.map((item) => (
+                     <ExperienceCard key={item.title} details={item} />
+                  ))}
+               </Slider>
             </div>
          </section>
       </>
