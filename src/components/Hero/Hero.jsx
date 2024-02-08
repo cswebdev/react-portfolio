ae9f8ed99edc683dbe81dfb50b3ce34a8cf4cd19
cswebdev <csnider91@live.com>
2024-02-08 12:47:43 -0500

    added links for social icons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
          <section className="hero-container">
             <div className="hero-content">
                <h2>
-                  Hello, <br /> I'm Chelsea!
+                  Hello, <br /> I'm Christian!
                </h2>
                <p>Full Stack Developer | Innovator | Problem Solver</p>
                <p>
@@ -21,13 +21,26 @@ const Hero = () => {
                </p>
                <div className="social-bar">
                   <div className="social-icon">
-                     <img src={linkedinIcon} alt="linkedIcon" />
+                     <a
+                        href="https://www.linkedin.com/in/chelsea-snider"
+                        target="_blank"
+                     >
+                        <img src={linkedinIcon} alt="linkedIcon" />
+                     </a>
                   </div>
                   <div className="social-icon">
-                     <img id="githubIcon" src={githubIcon} alt="githubIcon" />
+                     <a href="https://github.com/cswebdev" target="_blank">
+                        <img
+                           id="githubIcon"
+                           src={githubIcon}
+                           alt="githubIcon"
+                        />
+                     </a>
                   </div>
                   <div className="social-icon">
-                     <img id="githubIcon" src={devtoIcon} alt="devTo Icon" />
+                     <a href="https://dev.to/codewithchelsea" target="_blank">
+                        <img id="githubIcon" src={devtoIcon} alt="devTo Icon" />
+                     </a>
                   </div>
                </div>
             </div>
